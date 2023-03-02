4fe97f012be261fa4ceccd07b0b1779b505aca84
Leonardo Parente <leonardo.parente@encora.com>
2023-03-02 14:30:46 -0400

    Add first implementation for diode agent

---
+++ cmd/agent/main.go
@@ -0,0 +1,187 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/orb-community/diode/agent"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	defaultConfig = "/opt/diode/agent.yaml"
+)
+
+var (
+	cfgFiles []string
+	Debug    bool
+)
+
+func Run(cmd *cobra.Command, args []string) {
+
+	initConfig()
+
+	// configuration
+	var config config.Config
+	err := viper.Unmarshal(&config)
+	if err != nil {
+		cobra.CheckErr(fmt.Errorf("agent start up error (config): %w", err))
+		os.Exit(1)
+	}
+
+	config.OrbAgent.Debug.Enable = Debug
+
+	// logger
+	var logger *zap.Logger
+	atomicLevel := zap.NewAtomicLevel()
+	if Debug {
+		atomicLevel.SetLevel(zap.DebugLevel)
+	} else {
+		atomicLevel.SetLevel(zap.InfoLevel)
+	}
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderCfg),
+		os.Stdout,
+		atomicLevel,
+	)
+	logger = zap.New(core, zap.AddCaller())
+	defer func(logger *zap.Logger) {
+		_ = logger.Sync()
+	}(logger)
+
+	// new agent
+	a, err := agent.New(logger, config)
+	if err != nil {
+		logger.Error("agent start up error", zap.Error(err))
+		os.Exit(1)
+	}
+
+	// handle signals
+	done := make(chan bool, 1)
+	rootCtx, cancelFunc := context.WithCancel(context.WithValue(context.Background(), "routine", "mainRoutine"))
+
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+		select {
+		case <-sigs:
+			logger.Warn("stop signal received stopping agent")
+			a.Stop(rootCtx)
+			cancelFunc()
+		case <-rootCtx.Done():
+			logger.Warn("mainRoutine context cancelled")
+			done <- true
+			return
+		}
+	}()
+
+	// start agent
+	err = a.Start(rootCtx, cancelFunc)
+	if err != nil {
+		logger.Error("agent startup error", zap.Error(err))
+		os.Exit(1)
+	}
+
+	<-done
+}
+
+func mergeOrError(path string) {
+
+	v := viper.New()
+	if len(path) > 0 {
+		v.SetConfigFile(path)
+		v.SetConfigType("yaml")
+	}
+
+	v.AutomaticEnv()
+	replacer := strings.NewReplacer(".", "_")
+	v.SetEnvKeyReplacer(replacer)
+
+	// note: viper seems to require a default (or a BindEnv) to be overridden by environment variables
+	v.SetDefault("orb.cloud.api.address", "https://orb.live")
+	v.SetDefault("orb.cloud.api.token", "")
+	v.SetDefault("orb.cloud.config.agent_name", "")
+	v.SetDefault("orb.cloud.config.auto_provision", true)
+	v.SetDefault("orb.cloud.mqtt.address", "tls://agents.orb.live:8883")
+	v.SetDefault("orb.cloud.mqtt.id", "")
+	v.SetDefault("orb.cloud.mqtt.key", "")
+	v.SetDefault("orb.cloud.mqtt.channel_id", "")
+	v.SetDefault("orb.db.file", "./orb-agent.db")
+	v.SetDefault("orb.tls.verify", true)
+	v.SetDefault("orb.otel.enable", false)
+	v.SetDefault("orb.otel.receiver_type", "prometheus")
+	v.SetDefault("orb.debug.enable", false)
+
+	v.SetDefault("orb.backends.pktvisor.binary", "/usr/local/sbin/pktvisord")
+	v.SetDefault("orb.backends.pktvisor.config_file", "/opt/orb/agent.yaml")
+	v.SetDefault("orb.backends.pktvisor.api_host", "localhost")
+	v.SetDefault("orb.backends.pktvisor.api_port", "10853")
+
+	if len(path) > 0 {
+		cobra.CheckErr(v.ReadInConfig())
+	}
+
+	var fZero float64
+
+	// check that version of config files are all matched up
+	if versionNumber1 := viper.GetFloat64("version"); versionNumber1 != fZero {
+		versionNumber2 := v.GetFloat64("version")
+		if versionNumber2 == fZero {
+			cobra.CheckErr("Failed to parse config version in: " + path)
+		}
+		if versionNumber2 != versionNumber1 {
+			cobra.CheckErr("Config file version mismatch in: " + path)
+		}
+	}
+
+	cobra.CheckErr(viper.MergeConfigMap(v.AllSettings()))
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+
+	// set defaults first
+	mergeOrError("")
+	if len(cfgFiles) == 0 {
+		if _, err := os.Stat(defaultConfig); !os.IsNotExist(err) {
+			mergeOrError(defaultConfig)
+		}
+	} else {
+		for _, conf := range cfgFiles {
+			mergeOrError(conf)
+		}
+	}
+}
+
+func main() {
+
+	rootCmd := &cobra.Command{
+		Use: "orb-agent",
+	}
+
+	runCmd := &cobra.Command{
+		Use:   "run",
+		Short: "Run orb-agent and connect to Orb control plane",
+		Long:  `Run orb-agent and connect to Orb control plane`,
+		Run:   Run,
+	}
+
+	runCmd.Flags().StringSliceVarP(&cfgFiles, "config", "c", []string{}, "Path to config files (may be specified multiple times)")
+	runCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "Enable verbose (debug level) output")
+
+	rootCmd.AddCommand(runCmd)
+	rootCmd.Execute()
+}
