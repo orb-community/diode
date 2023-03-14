f09499e30bc02f706c1c7fbcf4728b671a4e71ed
Everton H. Taques <97463920+etaques@users.noreply.github.com>
2023-03-14 19:44:48 -0300

    Create run-agent.sh

---
+++ agent/docker/run-agent.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# diode agent binary location. by default, matches diode-agent container (see Dockerfile)
+diode_agent_bin="${DIODE_AGENT_BIN:-/usr/local/bin/diode-agent}"
+#
+if [ $# -eq 0 ]; then
+  "$diode_agent_bin" run &
+  echo $! > /var/run/diode-agent.pid
+else
+  "$diode_agent_bin" "$@" &
+  echo $! > /var/run/diode-agent.pid
+fi
