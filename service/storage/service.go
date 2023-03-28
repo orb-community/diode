31503a472a3f857142143727ee2457d316be8084
Luiz Pegoraro <pegorarohluiz@gmail.comw>
2023-03-28 15:47:24 -0300

    feat(storage): pass models and fields to storage.

--- service/storage/service.go
+++ service/storage/service.go
@@ -1,44 +1,44 @@
 package storage
 
 type Service interface {
-	Save(policy string, jsonData map[string]interface{}) (string, error)
+	Save(policy string, jsonData map[string]interface{}) (interface{}, error)
 }
 
 type DbInterface struct {
-	Id          string
-	Policy      string
-	Namespace   string
-	Hostname    string
-	Name        string
-	AdminState  string
-	Mtu         int64
-	Speed       int64
-	MacAddress  string
-	IfType      string
-	NetboxRefId int64
-	Blob        string
+	Id          string `json:"id,omitempty"`
+	Policy      string `json:"policy,omitempty"`
+	Namespace   string `json:"namespace"`
+	Hostname    string `json:"hostname"`
+	Name        string `json:"name"`
+	AdminState  string `json:"admin_state"`
+	Mtu         int64  `json:"mtu"`
+	Speed       int64  `json:"speed"`
+	MacAddress  string `json:"mac_address"`
+	IfType      string `json:"if_type"`
+	NetboxRefId int64  `json:"netbox_id,omitempty"`
+	Blob        string `json:"blob,omitempty"`
 }
 
 type DbDevice struct {
-	Id           string
-	Policy       string
-	SerialNumber string
-	Namespace    string
-	Hostname     string
-	Model        string
-	State        string
-	Vendor       string
-	NetboxRefId  int64
-	Blob         string
+	Id           string `json:"id,omitempty"`
+	Policy       string `json:"policy,omitempty"`
+	SerialNumber string `json:"serial_number"`
+	Namespace    string `json:"namespace"`
+	Hostname     string `json:"hostname"`
+	Model        string `json:"model"`
+	State        string `json:"state"`
+	Vendor       string `json:"vendor"`
+	NetboxRefId  int64  `json:"netbox_id,omitempty"`
+	Blob         string `json:"blob,omitempty"`
 }
 
 type DbVlan struct {
-	Id          string
-	Policy      string
-	Namespace   string
-	Hostname    string
-	Name        string
-	State       string
-	NetboxRefId int64
-	Blob        string
+	Id          string `json:"id,omitempty"`
+	Policy      string `json:"policy,omitempty"`
+	Namespace   string `json:"namespace"`
+	Hostname    string `json:"hostname"`
+	Name        string `json:"name"`
+	State       string `json:"state"`
+	NetboxRefId int64  `json:"netbox_id,omitempty"`
+	Blob        string `json:"blob,omitempty"`
 }
--- service/storage/sqliteconn.go
+++ service/storage/sqliteconn.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
 	"go.uber.org/zap"
@@ -22,46 +23,110 @@ func NewSqliteStorage(logger *zap.Logger) (Service, error) {
 	return sqliteStorage{db: db, logger: logger}, nil
 }
 
-func (s sqliteStorage) Save(policy string, jsonData map[string]interface{}) (id string, err error) {
+func (s sqliteStorage) Save(policy string, jsonData map[string]interface{}) (stored interface{}, err error) {
 	data, ok := jsonData["interfaces"].([]map[string]interface{})
 	if ok {
+		interfacesAdded := make([]DbInterface, len(data))
 		for _, interfaceData := range data {
 			dataAsString, err := json.Marshal(interfaceData)
 			if err != nil {
 				s.logger.Error("error marshalling interface data", zap.Error(err))
 				continue
 			}
-			id := uuid.NewString()
-			statement, err := s.db.Prepare("INSERT INTO interfaces (id, policy, interfaceData) VALUES (?,?,?)")
+			dbInterface := DbInterface{
+				Id:     uuid.NewString(),
+				Policy: policy,
+				Blob:   string(dataAsString),
+			}
+			err = json.Unmarshal(dataAsString, &dbInterface)
+			if err != nil {
+				s.logger.Error("error marshalling interface data", zap.Error(err))
+				continue
+			}
+			statement, err := s.db.Prepare(`
+			INSERT INTO interfaces 
+			    (
+			     id, 
+				 policy, 
+				 namespace,
+				 hostname,
+				 name,
+				 admin_state,
+				 mtu,
+				 speed,
+				 mac_address,
+				 if_type, 
+				 json_data
+			    ) VALUES 
+				  (
+				   $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11
+				  )
+			`)
 			if err != nil {
 				s.logger.Error("error during preparing insert statement on interface", zap.Error(err))
 				continue
 			}
-			statement.Exec(id, policy, dataAsString)
+			_, err = statement.Exec(dbInterface.Id, policy, dbInterface.Namespace, dbInterface.Hostname, dbInterface.Name,
+				dbInterface.AdminState, dbInterface.Mtu, dbInterface.Speed, dbInterface.MacAddress, dbInterface.IfType, dataAsString)
+			if err != nil {
+				s.logger.Error("error during preparing insert statement on interface", zap.Error(err))
+				continue
+			}
+			interfacesAdded = append(interfacesAdded, dbInterface)
 		}
 		if err != nil {
 			return "", err
 		}
+		return interfacesAdded, nil
 	}
 	data, ok = jsonData["device"].([]map[string]interface{})
-	dataAsString, err := json.Marshal(data)
-	if err != nil {
-		s.logger.Error("error marshalling interface data", zap.Error(err))
-		return "", err
+	if ok {
+		dataAsString, err := json.Marshal(data)
+		if err != nil {
+			s.logger.Error("error marshalling interface data", zap.Error(err))
+			return nil, err
+		}
+		dbDevice := DbDevice{
+			Id:     uuid.NewString(),
+			Policy: policy,
+			Blob:   string(dataAsString),
+		}
+		err = json.Unmarshal(dataAsString, &dbDevice)
+		if err != nil {
+			s.logger.Error("error marshalling interface data", zap.Error(err))
+			return nil, err
+		}
+		statement, err := s.db.Prepare(
+			`
+		INSERT INTO devices 
+		    (
+		 	id,
+		    policy, 
+		 	namespace,
+		 	hostname,
+		 	serial_number,
+		 	model,
+		 	state,
+		 	vendor, 
+		    json_data) 
+		VALUES 
+		    (
+		 	  $1, $2, $3, $4, $5, $6, $7, $8, $9
+		    )
+		`)
+		if err != nil {
+			s.logger.Error("error during preparing insert statement", zap.Error(err))
+			return "", err
+		}
+		_, err = statement.Exec(dbDevice.Id, policy, dbDevice.Namespace, dbDevice.Hostname, dbDevice.SerialNumber,
+			dbDevice.Model, dbDevice.State, dbDevice.Vendor, dataAsString)
+		if err != nil {
+			s.logger.Error("error during executing insert statement", zap.Error(err))
+			return "", err
+		}
+		return dbDevice, nil
 	}
-	id = uuid.NewString()
-	statement, err := s.db.Prepare("INSERT INTO devices (id, policy, interfaceData) VALUES (?,?,?)")
-	if err != nil {
-		s.logger.Error("error during preparing insert statement", zap.Error(err))
-		return "", err
-	}
-	_, err = statement.Exec(id, policy, dataAsString)
-	if err != nil {
-		s.logger.Error("error during executing insert statement", zap.Error(err))
-		return "", err
-	}
-
-	return id, nil
+	return nil, errors.New("not able to save anything from entry")
 }
 
 func startSqliteDb(logger *zap.Logger) (db *sql.DB, err error) {
@@ -75,7 +140,21 @@ func startSqliteDb(logger *zap.Logger) (db *sql.DB, err error) {
 		return nil, err
 	}
 
-	createInterfacesTableStatement, err := db.Prepare("CREATE TABLE IF NOT EXISTS interfaces (id TEXT PRIMARY KEY, policy TEXT, interfaceData TEXT )")
+	createInterfacesTableStatement, err := db.Prepare(`
+		CREATE TABLE IF NOT EXISTS interfaces 
+		( id TEXT PRIMARY KEY, 
+		 policy TEXT, 
+		 namespace TEXT,
+		 hostname TEXT,
+		 name TEXT,
+		 admin_state TEXT,
+		 mtu INTEGER,
+		 speed INTEGER,
+		 mac_address TEXT,
+		 if_type TEXT,
+		 netbox_id INTEGER NULL, 
+		 json_data TEXT )
+    `)
 	if err != nil {
 		logger.Error("error preparing interfaces statement", zap.Error(err))
 		return nil, err
@@ -86,7 +165,21 @@ func startSqliteDb(logger *zap.Logger) (db *sql.DB, err error) {
 		return nil, err
 	}
 	logger.Debug("successfully created Interfaces table")
-	createDeviceTableStatement, err := db.Prepare("CREATE TABLE IF NOT EXISTS devices (id TEXT PRIMARY KEY, policy TEXT, deviceData TEXT )")
+	createDeviceTableStatement, err := db.Prepare(`
+		CREATE TABLE IF NOT EXISTS devices 
+		(
+		    id TEXT PRIMARY KEY, 
+		 	policy TEXT, 
+		 	namespace TEXT,
+		 	hostname TEXT,
+		 	serial_number TEXT,
+		 	model TEXT,
+		 	state TEXT,
+		 	vendor TEXT,
+		 	netbox_id INTEGER NULL, 
+		    json_data TEXT 
+		)
+    `)
 	if err != nil {
 		logger.Error("error preparing devices statement ", zap.Error(err))
 		return nil, err
